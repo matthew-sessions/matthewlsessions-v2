e3b3030c8e20d8905d64423eafb785f1effe3a37
Matthew Sessions <53715422+matthew-sessions@users.noreply.github.com>
2021-10-10 11:31:59 -0700

    Update loadbalance_run.sh

--- loadbalance_run.sh
+++ loadbalance_run.sh
@@ -2,7 +2,5 @@
 
 cd /FastAPI_MongoDB
 
-python3 handleenv.py
-
 docker build -t fastapi .
 docker run -it -p 80:80 -v $(pwd)/app:/code/app fastapi uvicorn app.main:app --host 0.0.0.0 --port 80 --reload
