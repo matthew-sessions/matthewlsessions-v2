9dee05067dfe9ce7e2adc925a2ac5efb51824967
Matthew Sessions <msessions@expediagroup.com>
2023-10-09 20:31:26 -0700

    added refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,6 @@
 rm -r note-publish
 
 git clone https://github.com/matthew-sessions/note-publish.git
+
+docker-compose -f docker-compose-ssl.yml build
+docker-compose -f docker-compose-ssl.yml restart -d
