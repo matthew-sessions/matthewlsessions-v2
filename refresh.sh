b5b4a7f3c7060da8d5e7fffe90f918eb7cbf3ba1
Matthew Sessions <msessions@expediagroup.com>
2024-07-12 12:26:32 -0700

    no ssl

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,5 @@ rm -r note-publish
 
 git clone https://github.com/matthew-sessions/note-publish.git
 
-docker-compose -f docker-compose-ssl.yml build
-docker-compose -f docker-compose-ssl.yml restart -d
+docker-compose build
+docker-compose restart -d
